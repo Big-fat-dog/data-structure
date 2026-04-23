b7358caab4aa4a2a8239a2caf63355015c7a8f93
Fat Dog <2149436837@qq.com>
2026-04-23 10:43:02 +0800

    try

---
+++ leetcode/26.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow=0;
+        if(nums.empty()) return 0;
+        for(int fast=0;fast<nums.size();fast++){
+            if(nums[fast]!=nums[slow]){
+                slow++;
+                nums[slow]=nums[fast];
+            }
+            
+    }
+    return slow+1;
+    }
+};
