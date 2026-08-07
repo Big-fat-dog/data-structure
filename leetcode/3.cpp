93395de614575c58744599c756e3dc2a8ac8641c
Fat Dog <2149436837@qq.com>
2026-08-07 21:51:25 +0800

    8.7

---
+++ leetcode/3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size(),ans=0,left=0;
+        unordered_map<char,int> cnt;
+        for(int right=0;right<n;right++){
+            char c=s[right];
+            cnt[c]++;
+            while(cnt[c]>1){
+                cnt[s[left]]--;
+                left++;
+            }
+            ans = max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
