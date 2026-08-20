df1b5f0f96e2e1b58953ea92e65f38ad0b063211
Fat Dog <2149436837@qq.com>
2026-08-20 21:31:21 +0800

    8.20

---
+++ leetcode/206a.cpp
@@ -0,0 +1,25 @@
+
+ struct ListNode {
+     int val;
+      ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        //三指针法
+        if(head==nullptr||head->next==nullptr)return head;
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while(cur!=nullptr){
+            ListNode* lat = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = lat;
+        }
+        return pre;
+    }
+};
