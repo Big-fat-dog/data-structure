1eea61517f02329ee90dcfe60932a9ab7e520709
Fat Dog <2149436837@qq.com>
2026-09-05 23:09:11 +0800

    9.5

---
+++ leetcode/98.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+    bool isValidBST(TreeNode* root, long long left = LLONG_MIN, long long right = LLONG_MAX) {
+        if (root == nullptr) {
+            return true;
+        }
+        long long x = root->val;
+        return left < x && x < right &&
+               isValidBST(root->left, left, x) &&
+               isValidBST(root->right, x, right);
+    }
+};
+
