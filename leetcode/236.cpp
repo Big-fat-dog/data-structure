9f041493153f34f11e8fbc5c3a3fe5b4964df68d
Fat Dog <2149436837@qq.com>
2026-09-08 14:49:43 +0800

    9.8

---
+++ leetcode/236.cpp
@@ -0,0 +1,30 @@
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)return nullptr;
+        if(root==p||root==q)return root;//找到对应节点
+
+        //左子树递归
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+
+        //如果左右子树都能找到
+        if(left&&right){
+            return root;
+        }
+
+        return left?left:right;
+    }
+};
